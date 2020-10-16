c7075342715ec07426c2a347ffc230e0e1be53bd
Forrest Reiland <you@example.com>
2020-10-16 14:37:23 -0700

    add validation for rating on review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :product
   validates :author, presence: true
+  validates_length_of :content_body, maximum: 250
+  validates_length_of :content_body, minimum: 50
+  validates :rating, presence: { in: 1..5 }
 end
