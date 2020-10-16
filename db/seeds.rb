7966825363c347b9fdb2b4d3da76aa7b9abc2514
Forrest Reiland <you@example.com>
2020-10-16 13:47:45 -0700

    seed databse with faker beers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+125.times do |index|
+  Product.create!(name: Faker::Beer.name,
+                        cost: rand(1..25), country_of_origin: Faker::WorldCup.team)
+                        
+end
+
+p "Created #{Product.count} beers"
