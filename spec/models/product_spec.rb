29ba5e6238db61cf2eebeace883a7d17dc4dd309
Forrest Reiland <you@example.com>
2020-10-16 14:56:17 -0700

    add callback to titleize product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,4 +5,8 @@ describe Product do
   it { should validate_presence_of :name }
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
+  it("titleizes the name of an product") do
+    product = Product.create({name: "Maribou lager", cost: 22, country_of_origin: 'USA'})
+    expect(product.name()).to(eq("Maribou Lager"))
+  end
 end
